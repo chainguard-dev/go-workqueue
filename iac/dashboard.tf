34f12b6d33701de175b5690c8e1cc926066787e0
Matt Moore <mattmoor@chainguard.dev>
2024-08-22 20:24:31 -0700

    Add latency metrics to the dashboard (#10)
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

--- iac/dashboard.tf
+++ iac/dashboard.tf
@@ -25,7 +25,7 @@ module "work-in-progress" {
   source  = "chainguard-dev/common/infra//modules/dashboard/widgets/xy"
   version = "0.6.65"
 
-  title  = "Work In Progress"
+  title  = "Amount of work in progress"
   filter = [
     "resource.type=\"prometheus_target\"",
     "metric.type=\"prometheus.googleapis.com/workqueue_in_progress_keys/gauge\"",
@@ -34,13 +34,16 @@ module "work-in-progress" {
   group_by_fields = ["metric.label.\"service_name\""]
   primary_align   = "ALIGN_MAX"
   primary_reduce  = "REDUCE_MAX"
+
+  # TODO(mattmoor): Add threshold when it lands in a release.
+  # thresholds = [var.concurrent-work]
 }
 
 module "work-queued" {
   source  = "chainguard-dev/common/infra//modules/dashboard/widgets/xy"
   version = "0.6.65"
 
-  title  = "Work Queued"
+  title  = "Amount of work queued"
   filter = [
     "resource.type=\"prometheus_target\"",
     "metric.type=\"prometheus.googleapis.com/workqueue_queued_keys/gauge\"",
@@ -55,7 +58,7 @@ module "work-added" {
   source  = "chainguard-dev/common/infra//modules/dashboard/widgets/xy"
   version = "0.6.65"
 
-  title  = "Work Added"
+  title  = "Amount of work added"
   filter = [
     "resource.type=\"prometheus_target\"",
     "metric.type=\"prometheus.googleapis.com/workqueue_added_keys_total/counter\"",
@@ -68,25 +71,61 @@ module "work-added" {
   secondary_reduce = "REDUCE_SUM"
 }
 
-module "work-deduped" {
-  source  = "chainguard-dev/common/infra//modules/dashboard/widgets/xy"
+module "process-latency" {
+  source  = "chainguard-dev/common/infra//modules/dashboard/widgets/latency"
   version = "0.6.65"
 
-  title  = "Work Deduplicated"
+  title  = "Work processing latency"
   filter = [
+    "resource.type=\"prometheus_target\"",
+    "metric.type=\"prometheus.googleapis.com/workqueue_process_latency_seconds/histogram\"",
+    "metric.label.\"service_name\"=\"${var.name}-dsp\"",
+  ]
+}
+
+module "wait-latency" {
+  source  = "chainguard-dev/common/infra//modules/dashboard/widgets/latency"
+  version = "0.6.65"
+
+  title  = "Work wait times"
+  filter = [
+    "resource.type=\"prometheus_target\"",
+    "metric.type=\"prometheus.googleapis.com/workqueue_wait_latency_seconds/histogram\"",
+    "metric.label.\"service_name\"=\"${var.name}-dsp\"",
+  ]
+}
+
+module "percent-deduped" {
+  source  = "chainguard-dev/common/infra//modules/dashboard/widgets/xy-ratio"
+  version = "0.6.65"
+
+  title     = "Percentage of work deduplicated"
+  legend    = ""
+  plot_type = "LINE"
+
+  numerator_filter = [
     "resource.type=\"prometheus_target\"",
     "metric.type=\"prometheus.googleapis.com/workqueue_deduped_keys_total/counter\"",
     "metric.label.\"service_name\"=\"${var.name}-rcv\"",
   ]
-  group_by_fields = ["metric.label.\"service_name\""]
-  primary_align    = "ALIGN_RATE"
-  primary_reduce   = "REDUCE_NONE"
-  secondary_align  = "ALIGN_NONE"
-  secondary_reduce = "REDUCE_SUM"
+  denominator_filter = [
+    "resource.type=\"prometheus_target\"",
+    "metric.type=\"prometheus.googleapis.com/workqueue_added_keys_total/counter\"",
+    "metric.label.\"service_name\"=\"${var.name}-rcv\"",
+  ]
+
+  alignment_period            = "60s"
+  thresholds                  = []
+  numerator_align             = "ALIGN_RATE"
+  numerator_group_by_fields   = ["metric.label.\"service_name\""]
+  numerator_reduce            = "REDUCE_SUM"
+  denominator_align           = "ALIGN_RATE"
+  denominator_group_by_fields = ["metric.label.\"service_name\""]
+  denominator_reduce          = "REDUCE_SUM"
 }
 
 locals {
-  columns = 2
+  columns = 3
   unit    = module.width.size / local.columns
 
   // https://www.terraform.io/language/functions/range
@@ -109,18 +148,32 @@ locals {
       widget = module.work-queued.widget,
     },
     {
-      yPos   = local.unit,
-      xPos   = local.col[0],
+      yPos   = 0,
+      xPos   = local.col[2],
       height = local.unit,
       width  = local.unit,
       widget = module.work-added.widget,
     },
     {
       yPos   = local.unit,
+      xPos   = local.col[0],
+      height = local.unit,
+      width  = local.unit,
+      widget = module.process-latency.widget,
+    },
+    {
+      yPos   = local.unit,
       xPos   = local.col[1],
       height = local.unit,
       width  = local.unit,
-      widget = module.work-deduped.widget,
+      widget = module.wait-latency.widget,
+    },
+    {
+      yPos   = local.unit,
+      xPos   = local.col[2],
+      height = local.unit,
+      width  = local.unit,
+      widget = module.percent-deduped.widget,
     },
   ]
 }
